5b660cf6c5c7168dd2926c65bc8c446243925ae0
Rafael Monteiro <rafaelmonteiroti@gmail.com>
2016-10-27 12:30:03 -0300

    adding pins.

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,10 +1,17 @@
 class PinsController < ApplicationController
+  before_action :find_pin, only: [:show, :edit, :update, :destroy]
+
 	def index
-	end 
+    @pins = Pin.all.order("created_at DESC")
+	end
+
+  def show
+  end
 
 	def new 
     @pin = Pin.new
 	end
+
 	def create
     @pin = Pin.new(pin_params)
     if @pin.save
@@ -14,9 +21,34 @@ class PinsController < ApplicationController
     end
 	end
 
-private 
+  def edit
+  end
+
+  def update
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: "Pin was Successfully updated!"
+    else
+      render 'edit'
+      end  
+  end 
+
+  def destroy
+    @pin.destroy
+    redirect_to root_path
+  end 
+
+  
+
+
+private
+
 	def pin_params
-	params.require(:pin).permit(:title, :description)
+    params.require(:pin).permit(:title, :description)
 	end
+
+  def find_pin
+    @pin = Pin.find(params[:id])
+  end
+
 
 end
